d36d5b9bf4567fd417b21ab079f901540f7a9688
stdpi <stdpi@proton.me>
2024-02-15 04:57:27 +0700

    init

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import "./styles/main.sass"
+import "./styles/bg.sass"
+import "./styles/pages.sass"
+
+window.addEventListener('load', function () {
+    this.setTimeout(() => {
+        document.body.className += "loaded";
+        let state = false
+        this.setInterval(() => {
+            state ? this.document.body.style.setProperty("--e", '0') : this.document.body.style.setProperty("--e", '0.0004')
+            state = !state
+        }, 1500)
+    }, 100)
+})
